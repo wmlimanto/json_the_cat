6a6d6250f647ef4c1d311f9a1af1d82a142ec597
Winston Limanto <wmlimanto@gmail.com>
2022-04-22 18:15:35 -0700

    exported fetchBreedDescription function to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,21 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breed = args[0];
 
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  // edge case: request failed
-  if (error) {
-    console.log("URL is invalid!");
-    return;
-    // edge case: breed not found
-  } else if (body === '[]') {
-    console.log(`We can't find this breed - please try again!`);
-    return;
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-    return;
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    // edge case: request failed
+    if (error) {
+      callback(error, null);
+      // edge case: breed not found
+    } else if (body === '[]') {
+      callback(`We can't find this breed - please try again!`, null)
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  })
+};
+
+module.exports = { fetchBreedDescription };
